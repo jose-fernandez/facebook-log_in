b0f8248200e67c934ce0140a7a59fd4a79938f61
Jose Fernandez <jose.fernandez91@gmail.com>
2018-11-09 21:11:42 +0100

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# facebook_login
+This is simple Log In SPA with Facebook
+
+## Installation
+```
+$ yarn
+```
+
+## Stack
+- vue
+    -vue@2.3.3
+    -vuex@2.3.1
+- Build
+    - webpack@3.0.0
+    - webpack-dev-server@2.4.5
+    - babel-preset-env@1.4.0
+    - babel-preset-react@6.24.1
+    - babel-preset-stage-0@6.24.1
+
+## Default Commands
+```
+$ yarn dev    // lunch webpack-dev-server
+$ yarn build  // build bundle files
+```
+
+### Boilerplate used
+[vue-vuex-boilerplate](https://github.com/hg-pyun/vue-vuex-boilerplate).
---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+    count: 0
+};
+
+const mutations = {
+    increment (state) {
+        state.count++;
+    },
+    decrement (state) {
+        state.count--;
+    }
+};
+
+const actions = {
+    increment: ({ commit }) => commit('increment'),
+    decrement: ({ commit }) => commit('decrement')
+};
+
+const getters = {
+    evenOrOdd: state => state.count % 2 === 0 ? 'even' : 'odd'
+};
+
+export default new Vuex.Store({
+    state,
+    getters,
+    actions,
+    mutations
+})
